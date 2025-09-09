7b57ef441f91683b987e192d650df481b2c09eeb
divyareddy098 <divyareddypeddi@gmail.com>
2025-09-09 13:13:03 -0400

    find_homologs.sh

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-makeblastdb -in "$2" -dbtype nucl-out subjdb
-tblastn -query "$1" -db subjdb -outfmt "6 qseqid sseqid pident length qlen" › raw.tsv
-awk $3 > 30 8& $4 >= (0.9 * 85) raw.tsv > "§3"
+makeblastdb -in "$2" -dbtype nucl -out subjdb
+tblastn -query "$1" -db subjdb -outfmt 6 -out raw.tsv
+awk '$3 > 30 && $4 >= 0.9 * $5' raw.tsv > "$3"
 wC -1 < "$3"
