cd2d60746cc7a265d14b1603f4ca03b9a4ded0c6
divyareddy098 <divyareddypeddi@gmail.com>
2025-09-09 13:36:53 -0400

    find_homologs.sh

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-makeblastdb -in "$2" -dbtype nucl -out subjdb
-tblastn -query "$1" -db subjdb -outfmt "6 qseqid sseqid pident length qlen" -out raw.tsv
-awk '$3 > 30 && $4 >= 0.9 * $5' raw.tsv > "$3"
-awk 'END {print NR}' "$3"
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <query file> <subject file> <output file>"
+    exit 1
+fi
+QUERY="$1"
+SUBJECT="$2"
+OUTPUT="$3"
+makeblastdb -in "$SUBJECT" -dbtype nucl -out subjdb >/dev/null 2>&1
+tblastn -query "$QUERY" -db subjdb -outfmt "6 qseqid sseqid pident length qlen" -out raw.tsv
+awk '$3 > 30 && $4 >= 0.9 * $5' raw.tsv > "$OUTPUT"
+awk 'END {print NR}' "$OUTPUT"
