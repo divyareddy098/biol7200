b4d2f96a6ce512e4a77a51a8bd6296f78d5b9d15
divyareddy098 <divyareddypeddi@gmail.com>
2025-09-09 09:40:43 +0530

    Update find_homologs.sh

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,3 +1,5 @@
-echo "#!/bin/bash" > find_homologs.sh
-git add find_homologs.sh
-git commit -m "Add shebang line to find_homologs.sh"
+#!/bin/bash
+makeblastdb -in "$2" -dbtype nucl-out subjdb
+tblastn -query "$1" -db subjdb -outfmt "6 qseqid sseqid pident length qlen" › raw.tsv
+awk $3 > 30 8& $4 >= (0.9 * 85) raw.tsv > "§3"
+wC -1 < "$3"
